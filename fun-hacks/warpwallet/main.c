2ad1d84ad59c7649fdbf742e8ec61bb84e57ac79
Calvin Owens <jcalvinowens@gmail.com>
2013-11-28 23:01:50 -0600

    Fix algorithm: 'password||0x1' means to appent 0x01 to the password

--- fun-hacks/warpwallet/main.c
+++ fun-hacks/warpwallet/main.c
@@ -25,10 +25,14 @@ do { \
 int crypto_scrypt(const uint8_t *passwd, size_t passwdlen, const uint8_t *salt, size_t saltlen, uint64_t N, uint32_t r, uint32_t p, uint8_t *buf, size_t buflen);
 void PBKDF2_SHA256(const uint8_t *passwd, size_t passwdlen, const uint8_t *salt, size_t saltlen, uint64_t c, uint8_t *buf, size_t dkLen);
 
-static void get_keypair_generator(char *password, char *salt, char *buf1, char *buf2, char *out)
+static void get_keypair_generator(char *password, int pw_len, char *salt, int sl_len, char *buf1, char *buf2, char *out)
 {
-	crypto_scrypt((uint8_t*) password, strlen(password), (uint8_t*) salt, strlen(salt), 262144, 8, 1, (uint8_t*) buf1, 32);
-	PBKDF2_SHA256((uint8_t*) password, strlen(password), (uint8_t*) salt, strlen(salt), 65536, (uint8_t*) buf2, 32);
+	password[pw_len] = '\x01';
+	salt[sl_len] = '\x01';
+	crypto_scrypt((uint8_t*) password, pw_len + 1, (uint8_t*) salt, sl_len + 1, 262144, 8, 1, (uint8_t*) buf1, 32);
+	password[pw_len] = '\x02';
+	salt[sl_len] = '\x02';
+	PBKDF2_SHA256((uint8_t*) password, pw_len + 1, (uint8_t*) salt, sl_len + 1, 65536, (uint8_t*) buf2, 32);
 
 	((uint64_t*) out)[0] = ((uint64_t*) buf1)[0] ^ ((uint64_t*) buf2)[0];
 	((uint64_t*) out)[1] = ((uint64_t*) buf1)[1] ^ ((uint64_t*) buf2)[1];
@@ -47,11 +51,19 @@ int main(int nargs, char **argv)
 		return -1;
 	}
 
-	get_keypair_generator(argv[1], argv[2], s1, s2, s3);
+	int pw_len = strlen(argv[1]);
+	char *pw = calloc(pw_len + 1, 1);
+	strcpy(pw, argv[1]);
 
-//	printf("%s %s\n", argv[1], argv[2]);
-//	PRINT_HEX_BLOB(s1,32);
-//	PRINT_HEX_BLOB(s2,32);
+	int sl_len = strlen(argv[2]);
+	char *sl = calloc(sl_len + 1, 1);
+	strcpy(sl, argv[2]);
+
+	pw[pw_len] = '\xff';
+	sl[sl_len] = '\xff';
+
+	get_keypair_generator(pw, pw_len, sl, sl_len, s1, s2, s3);
+
 	PRINT_HEX_BLOB(s3,32);
 
 	return 0;
